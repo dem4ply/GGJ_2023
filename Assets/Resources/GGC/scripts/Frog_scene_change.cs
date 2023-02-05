dbb66852bc77e68ab4e70e04640dba11e42580ff
dem4ply <dem4ply@gmail.com>
2023-02-05 00:15:32 -0600

    cambio de scene

---
+++ Assets/Resources/GGC/scripts/Frog_scene_change.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using chibi.controller.npc;
+using System.Collections.Generic;
+using platformer.controller.platform;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+namespace GGC.scene
+{
+	public class Frog_scene_change: chibi.Chibi_behaviour
+	{
+		public int scene_index;
+		private void OnTriggerEnter( Collider other )
+		{
+			SceneManager.LoadScene( scene_index );
+		}
+	}
+}
